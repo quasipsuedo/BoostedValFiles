9febdd0b071ac9bb1278a98cb853ccd3211093b9
reckon <lastyearshero@gmail.com>
2021-05-17 08:48:14 +1000

    Configs

---
+++ config/Notes.pl
@@ -0,0 +1,77 @@
+﻿60, 28, 12, 0
+55, 31, 14, 0
+50, 34, 16, 0
+
+UpirGrim
+ZaineEvilian
+
+LazarusAutumn
+GrailThornheart
+
+CrisenthShadowsoul
+JaydenShadowmend
+
+LuxFrost
+FirionWinter
+
+EchoBlack
+MathianSerphent
+
+SceledrusShadowend
+LazarusDeamonne
+
+LincolnHunt
+DravenNox
+
+AshVexx
+CinderMortem
+
+AtgeirBlackmetal
+AxeBlackMetal
+KnifeBlackMetal
+SwordBlackmetal
+MaceNeedle
+BowDraugrFang
+MaceSilver
+SwordIronFire
+
+﻿[WorldSpawner.0]
+Name = deer
+Enabled = true
+Biomes = Meadows,BlackForest,
+PrefabName = Deer
+HuntPlayer = false
+SetFaction = 
+MaxSpawned = 4
+SpawnInterval = 100
+SpawnChance = 50
+LevelMin = 1
+LevelMax = 3
+LevelUpMinCenterDistance = 0
+SpawnDistance = 64
+SpawnRadiusMin = 0
+SpawnRadiusMax = 0
+RequiredGlobalKey = 
+RequiredNotGlobalKey = 
+RequiredEnvironments = 
+GroupSizeMin = 1
+GroupSizeMax = 3
+GroupRadius = 5
+GroundOffset = 0.5
+SpawnDuringDay = true
+SpawnDuringNight = true
+ConditionAltitudeMin = 0
+ConditionAltitudeMax = 1000
+ConditionTiltMin = 0
+ConditionTiltMax = 35
+SpawnInForest = true
+SpawnOutsideForest = false
+OceanDepthMin = 0
+OceanDepthMax = 0
+ConditionDistanceToCenterMin = 0
+ConditionDistanceToCenterMax = 0
+ConditionWorldAgeDaysMin = 0
+ConditionWorldAgeDaysMax = 0
+DistanceToTriggerPlayerConditions = 100
+ConditionNearbyPlayersCarryValue = 0
+ConditionNearbyPlayerCarriesItem = 
